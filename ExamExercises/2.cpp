5f13f6c2ff7f9055e6d769c1b01a700d218f6ffb
Aleksandar Tashev <117026452+tashev29@users.noreply.github.com>
2024-08-20 14:16:39 +0200

    Update 2.cpp

--- ExamExercises/2.cpp
+++ ExamExercises/2.cpp
@@ -1,3 +1,35 @@
 //На стандарден влез се дадени повеќе редови со броеви така што секој ред започнува со еден цел број (N>=1) што означува колку броеви следуваат по него во истиот ред. 
 //Да се напише програма која на СИ за секој ред ќе го испечати бројот со најголема најзначајна цифра. 
 //Читањето на броеви завршува кога ќе се прочита бројот 0.
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+int getMostSignificantDigit(int num) {
+    while (num >= 10) {
+        num /= 10;
+    }
+    return num;
+}
+int main() {
+    int n;
+    
+    while (cin >> n && n != 0) {
+        vector<int> numbers(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> numbers[i];
+        }
+        int maxDigit = -1;
+        int resultNumber = -1;
+        for (int num : numbers) {
+            int digit = getMostSignificantDigit(num);
+            if (digit > maxDigit) {
+                maxDigit = digit;
+                resultNumber = num;
+            }
+        }
+        cout << resultNumber << endl;
+    }
+    return 0;
+}
